cce5c9ef8bb1978e31b34a9d99d40e576259b0db
Unknown <jcar06332@gmail.com>
2017-08-11 02:19:53 +0800

    added ew to indicators

--- indicators.py
+++ indicators.py
@@ -11,8 +11,15 @@ def band_n(df,n):
     df['band_'+str(n)+'+2']=df['sma'+str(n)]+2*df['std'+str(n)]
     df['band_'+str(n)+'-1']=df['sma'+str(n)]-df['std'+str(n)]
     df['band_'+str(n)+'-2']=df['sma'+str(n)]-2*df['std'+str(n)]
-def ema_n(df,n): #check what span is
-    df['ema'+str(n)]=df['sma'+str(n)].ewm(span=n).mean()
+def ema_n(df,n):
+    df['ewma'+str(n)]=pd.ewma(df['Vwap (LTCUSD)'],span=n,min_periods=n)
+def ewstd_n(df,n):
+    df['ewstd'+str(n)]=pd.ewmstd(df['Vwap (LTCUSD)'],span=n,min_periods=n)
+def xband_n(df,mean,std):
+    df[mean+std+'_band_+1']=df[mean]+df[std]
+    df[mean+std+'_band_+2']=df[mean]+2*df[std]
+    df[mean+std+'_band_-1']=df[mean]-df[std]
+    df[mean+std+'_band_-2']=df[mean]-2*df[std]
 def slope(df,indi):
     df['slope_'+indi]=df[indi].diff()
 
